00e57608247e46ae5d93e47ed9b0269fb57639a8
joao <joaobrilhante2019@gmail.com>
2022-01-13 23:15:30 -0100

    game in home page
    show games in the home page (not tested).

---
+++ trabalho_final/lib/Services/GameService.dart
@@ -0,0 +1,17 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:trabalho_final/models/GameHome.dart';
+import 'package:trabalho_final/utilities/constants.dart';
+
+class GameService {
+  Future getGames() async{
+    final response = await http.get(Uri.parse(urlRawgDefault));
+
+    if(response.statusCode == 200){
+      List<GameHome> gameHome = jsonDecode(response.body)
+      ['results'].map<GameHome>((jsonItem) => GameHome.fromJson(jsonItem)).toList();
+    }else{
+      throw Exception("unenable to load games");
+    }
+  }
+}
--- trabalho_final/lib/routes/homepage.dart
+++ trabalho_final/lib/routes/homepage.dart
@@ -1,4 +1,8 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
+import 'package:trabalho_final/Services/GameService.dart';
+import 'package:trabalho_final/models/GameHome.dart';
 
 class Homepage extends StatefulWidget {
   @override
@@ -7,16 +11,85 @@ class Homepage extends StatefulWidget {
 
 class _Homepage extends State<Homepage> {
   @override
-  void initState() {}
+  var gamesInHomePage = <GameHome>[];
+  void initState() {
+    _getGameHome();
+    super.initState();
+  }
+
   var _formKey = GlobalKey<FormState>();
   TextEditingController tempController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Color(0xFF242D3C), 
+        backgroundColor: Color(0xFF242D3C),
         body: ListView(
-          children: []
-        ));
+            children: List.generate(
+          gamesInHomePage.length,
+          (index) {
+            return GameDisplay(
+              game: gamesInHomePage[index],
+              content: context);
+          },
+        )
+      )
+    );
+  }
+
+  Future<void> _getGameHome() async{
+    GameService gameService = new GameService();
+    var newGames = await gameService.getGames();
+    setState(() {
+      gamesInHomePage = newGames;
+    });
   }
 }
+
+class GameDisplay extends StatelessWidget{
+  const GameDisplay({
+    Key?key,
+    required this.game,
+    required this.content,
+  }) : super(key : key);
+
+  final GameHome game;
+  final BuildContext content;
+
+  @override
+  Widget build(BuildContext context){
+    String imageUrl;
+    if(game.gameBackgroundImage.isEmpty){
+      imageUrl = 'assets/images/image_not_found.png';
+    }else{
+      imageUrl = game.gameBackgroundImage;
+    }
+      return Container(
+    child: Column(
+      children: [
+        Image.network(imageUrl,
+        width: MediaQuery.of(context).size.width*0.35,
+        height: MediaQuery.of(context).size.height*.14,
+        ),
+        Text(
+          game.gameTitle,
+          style: TextStyle(
+            fontSize: 20,
+            fontFamily: 'Poppins',
+            fontWeight: FontWeight.w400,
+            color: Colors.white,
+          ),
+        ),
+      ],
+    ),
+      );
+
+
+
+
+  }
+
+
+
+}
+
